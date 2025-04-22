194961f3f2f5de45673ae450eacb1b725c6aeb0d
ELIAS GUILHERME SANTOS SILVA <guisilvafsa3@gmail.com>
2025-04-22 16:30:00 +0000

    Desafio SuperTrunfo NOVATO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -86,6 +86,7 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    //
 
     return 0;
 }
